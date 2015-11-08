f9e2a642e60e68a3692eace4522535af6f8d43bf
Haihong Wang <rainbow.wh@gmail.com>
2015-11-07 20:10:06 -0800

    Initial Commit

---
+++ Mailbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mailbox
+//
+//  Created by Haihong Wang on 11/7/15.
+//  Copyright © 2015 Haihong Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
