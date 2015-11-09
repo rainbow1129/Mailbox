7b19572e991c101d59b22f8706b09ada296bbe7f
Haihong Wang <rainbow.wh@gmail.com>
2015-11-08 16:25:06 -0800

    added archive and delete

--- Mailbox/MailBoxViewController.swift
+++ Mailbox/MailBoxViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class MailBoxViewController: UIViewController {
     
+    @IBOutlet weak var archiveIcon: UIImageView!
+    @IBOutlet weak var deleteIcon: UIImageView!
+    @IBOutlet weak var rescheduleView: UIImageView!
+    @IBOutlet weak var listView: UIImageView!
+    @IBOutlet weak var menuView: UIImageView!
     @IBOutlet weak var listIcon: UIImageView!
     @IBOutlet weak var laterIcon: UIImageView!
     
@@ -35,6 +40,9 @@ class MailBoxViewController: UIViewController {
         leftBackground.alpha = 0
         laterIcon.alpha = 0
         listIcon.alpha = 0
+        menuView.alpha = 0
+        listView.alpha = 0
+        rescheduleView.alpha = 0
         
         // Do any additional setup after loading the view.
     }
@@ -67,12 +75,16 @@ class MailBoxViewController: UIViewController {
                     leftBackground.backgroundColor = grayColor
                     laterIcon.alpha = (-diff/60)
                     listIcon.alpha = 0
+                    archiveIcon.alpha = 0
+                    deleteIcon.alpha = 0
                     
                 } else if (diff > -260) {
                     leftBackground.backgroundColor = yellowColor
                     laterIcon.center = CGPoint(x: (initialCenter.x + translation.x + 190), y: laterIcon.center.y)
                     laterIcon.alpha = 1
                     listIcon.alpha = 0
+                    archiveIcon.alpha = 0
+                    deleteIcon.alpha = 0
                     print(laterIcon.frame.origin)
                     
                 } else {
@@ -80,12 +92,38 @@ class MailBoxViewController: UIViewController {
                     listIcon.center = CGPoint(x: (initialCenter.x + translation.x + 190), y: listIcon.center.y)
                     laterIcon.alpha = 0
                     listIcon.alpha = 1
+                    archiveIcon.alpha = 0
+                    deleteIcon.alpha = 0
                 }
-
+                
                 
             } else {
                 self.rightBackground.alpha = 1
                 self.leftBackground.alpha = 0
+                if (diff < 60) {
+                    rightBackground.backgroundColor = grayColor
+                    archiveIcon.alpha = (diff/60)
+                    deleteIcon.alpha = 0
+                    laterIcon.alpha = 0
+                    listIcon.alpha = 0
+                    
+                } else if (diff < 260) {
+                    rightBackground.backgroundColor = greenColor
+                    archiveIcon.center = CGPoint(x: (initialCenter.x + translation.x - 190), y: laterIcon.center.y)
+                    archiveIcon.alpha = 1
+                    deleteIcon.alpha = 0
+                    laterIcon.alpha = 0
+                    listIcon.alpha = 0
+                    
+                } else {
+                    rightBackground.backgroundColor = redColor
+                    deleteIcon.center = CGPoint(x: (initialCenter.x + translation.x - 190), y: laterIcon.center.y)
+                    archiveIcon.alpha = 0
+                    deleteIcon.alpha = 1
+                    laterIcon.alpha = 0
+                    listIcon.alpha = 0
+                }
+                
             }
         } else if (sender.state == UIGestureRecognizerState.Ended) {
             let diff = self.messageView.center.x - self.view.center.x
@@ -96,31 +134,99 @@ class MailBoxViewController: UIViewController {
                     //  If released at this point, the message should return to its initial position.
                     UIView.animateWithDuration(0.2, animations: { () -> Void in
                         self.messageView.frame.origin.x = 0
-                        self.laterIcon.alpha = 0
-                        
                     })
                 } else if (diff > -260) {
                     // Upon release, the message should continue to reveal the yellow background. When the animation it complete, it should show the reschedule options.
-                    UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 10, options: [], animations: { () -> Void in
+                    UIView.animateWithDuration(0.3, delay: 0.3, usingSpringWithDamping: 0.7, initialSpringVelocity: 20, options: [], animations: { () -> Void in
                         self.messageView.frame.origin.x = -320
                         self.laterIcon.frame.origin.x = 30
                         self.laterIcon.alpha = 0
                         }, completion: { (finished: Bool) -> Void in
                             UIView.animateWithDuration(0.3, animations: { () -> Void in
-                              self.performSegueWithIdentifier("rescheduleSegue", sender: self)
+                                self.rescheduleView.alpha = 1
                             })
                     })
                 } else {
                     // Upon release, the message should continue to reveal the brown background. When the animation it complete, it should show the list options.
+                    UIView.animateWithDuration(0.3, delay: 0.3, usingSpringWithDamping: 0.7, initialSpringVelocity: 20, options: [], animations: { () -> Void in
+                        self.messageView.frame.origin.x = -320
+                        self.listIcon.frame.origin.x = 30
+                        self.listIcon.alpha = 0
+                        }, completion: { (finished: Bool) -> Void in
+                            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                                self.listView.alpha = 1
+                            })
+                    })
                 }
                 
             } else {
+                self.rightBackground.alpha = 1
+                self.leftBackground.alpha = 0
+                if (diff < 60) {
+                    //If released at this point, the message should return to its initial position.
+                    UIView.animateWithDuration(0.2, animations: { () -> Void in
+                        self.messageView.frame.origin.x = 0
+                    })
+                    
+                } else if (diff < 260) {
+                    //Upon release, the message should continue to reveal the green background. When the animation it complete, it should hide the message.
+                    UIView.animateWithDuration(0.3, delay: 0.3, usingSpringWithDamping: 0.9, initialSpringVelocity: 10, options: [], animations: { () -> Void in
+                        self.messageView.frame.origin.x = 320
+                        self.archiveIcon.frame.origin.x = 290
+                        self.archiveIcon.alpha = 0
+                        }, completion: { (finished: Bool) -> Void in
+                            UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut , animations: { () -> Void in
+                                self.feed.frame.origin.y = 0
+                                self.rightBackground.alpha = 0
+                                self.leftBackground.alpha = 0
+                                }, completion: { (finished: Bool) -> Void in
+                                    
+                            })
+                            
+                    })
+                    
+                } else {
+                    //Upon release, the message should continue to reveal the green background. When the animation it complete, it should hide the message.
+                    UIView.animateWithDuration(0.3, delay: 0.3, usingSpringWithDamping: 0.9, initialSpringVelocity: 10, options: [], animations: { () -> Void in
+                        self.messageView.frame.origin.x = 320
+                        self.deleteIcon.frame.origin.x = 290
+                        self.deleteIcon.alpha = 0
+                        }, completion: { (finished: Bool) -> Void in
+                            UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut , animations: { () -> Void in
+                                self.feed.frame.origin.y = 0
+                                self.rightBackground.alpha = 0
+                                self.leftBackground.alpha = 0
+                                }, completion: { (finished: Bool) -> Void in
+                                    
+                            })
+                            
+                    })
+                }
+                
                 
             }
         }
     }
     
+    @IBAction func didTapReschedule(sender: UITapGestureRecognizer) {
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 5, options: [], animations: { () -> Void in
+            self.rescheduleView.alpha = 0
+            self.feed.frame.origin.y = 0
+            self.rightBackground.alpha = 0
+            self.leftBackground.alpha = 0
+            }, completion: { (finished: Bool) -> Void in
+        })
+    }
     
+    @IBAction func didTapList(sender: UITapGestureRecognizer) {
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 5, options: [], animations: { () -> Void in
+            self.listView.alpha = 0
+            self.feed.frame.origin.y = 0
+            self.rightBackground.alpha = 0
+            self.leftBackground.alpha = 0
+            }, completion: { (finished: Bool) -> Void in
+        })
+    }
     
     /*
     // MARK: - Navigation
